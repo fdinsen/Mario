691e5d149442f784a564e1c526bc9d454087b496
Frederik Dinsen <gammarik@gmail.com>
2019-10-22 09:20:23 +0200

    Added project

---
+++ src/marioopgave/MarioOpgave.java
@@ -0,0 +1,14 @@
+package marioopgave;
+
+/**
+ *
+ * @author <Frederik Keis Dinsen>
+ */
+
+public class MarioOpgave {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
