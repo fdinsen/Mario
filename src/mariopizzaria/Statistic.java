619ed00f3cc7592bc3ec1e736bab13ffb7ee12f1
Frederik Dinsen <gammarik@gmail.com>
2019-10-25 09:28:11 +0200

    Fixed testDiscount

--- src/mariopizzaria/Statistic.java
+++ src/mariopizzaria/Statistic.java
@@ -147,4 +147,9 @@ public final class Statistic {
         }
     }
 
+    
+    public static String getStatistics() {
+        //TODO build a String that contains everything, using StringBuilder
+        return "";
+    }
 }
