3dc46f962a014bb1ef95329eb557fcad133c0384
rodedrengen <simon.588.jorgensen@gmail.com>
2019-10-28 14:33:14 +0100

    Singleton UI classes

--- src/mariopizzaria/StatisticsUI.java
+++ src/mariopizzaria/StatisticsUI.java
@@ -1,7 +1,20 @@
 
-
 package mariopizzaria;
 
 public class StatisticsUI {
 
+    private static StatisticsUI staticstics_UI_instance = null; 
+    
+    
+    private StatisticsUI(){
+        
+    }
+    
+    public static StatisticsUI getInstance() 
+    { 
+        if (staticstics_UI_instance == null) 
+            staticstics_UI_instance = new StatisticsUI(); 
+  
+        return staticstics_UI_instance; 
+    } 
 }
