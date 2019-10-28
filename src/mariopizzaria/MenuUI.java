b20194d51938c1d3f61f88eb925883833816ca5f
rodedrengen <simon.588.jorgensen@gmail.com>
2019-10-28 14:37:35 +0100

    Singleton UI MenuUI

--- src/mariopizzaria/MenuUI.java
+++ src/mariopizzaria/MenuUI.java
@@ -1,7 +1,18 @@
-
-
 package mariopizzaria;
 
 public class MenuUI {
 
+    private static MenuUI menu_Ui_Instance = null; 
+    
+    
+    private MenuUI(){
+    }
+    
+    public static MenuUI getInstance() 
+    { 
+        if (menu_Ui_Instance == null) 
+            menu_Ui_Instance = new MenuUI(); 
+  
+        return menu_Ui_Instance; 
+    } 
 }
